c47a4ab8c283cbab27a86006fbd38a98aabd607c
Matej Stupljanec <matej.stupljanec@gmail.com>
2026-03-29 22:05:05 +0200

    header update

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,5 +1,7 @@
 export default function Header() {
   return (
-    <div className="flex items-center justify-end text-3xl bg-amber-300 p-2">Matej M</div>
-  )
+    <div className="flex items-center justify-end text-3xl bg-amber-300 p-2">
+      Matej M
+    </div>
+  );
 }
